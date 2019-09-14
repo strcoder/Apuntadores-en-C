8f041ac4f78cf62a5fd5efb64e49e0a52a482ac1
Antonio Guzman <aguzmanv09@gmail.com>
2019-09-14 10:23:18 -0500

    Agregando las cabeceras de las funciones

--- operaciones.c
+++ operaciones.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
+void suma (double *a, double *b, double *c);
+void resta (double *a, double *b, double *c);
+void multiplicacion (double *a, double *b, double *c);
+void division (double *a, double *b, double *c);
+void factorial (double *a, double *c);
+
 int main(int argc, char const *argv[])
 {
     
     return 0;
+}
+
+void suma (double *a, double *b, double *c) 
+{
+
+}
+
+void resta (double *a, double *b, double *c) 
+{
+
+}
+
+void multiplicacion (double *a, double *b, double *c) 
+{
+
+}
+
+void division (double *a, double *b, double *c) 
+{
+
+}
+
+void factorial (double *a, double *c) 
+{
+
 }
