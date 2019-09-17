aa2dc5e3636ad45b1171e43cc4c6e8afb8d56e33
Antonio Guzmán <agvcoder@gmail.com>
2019-09-17 09:42:26 -0500

    Codigo referente al manejo de la memoria dinamica

--- Memoria_dinamica.c
+++ Memoria_dinamica.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct
 {
@@ -6,6 +7,7 @@ typedef struct
     int m, n;
 }MATRIZ;
 
+MATRIZ asignar_memoria(int N);
 void alojar_memoria(MATRIZ *matriz, int m, int n);
 void leer_matriz(MATRIZ *matriz);
 void llenar_matriz(MATRIZ *matriz);
@@ -17,14 +19,34 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void alojar_memoria(MATRIZ *matriz, int m, int n)
+/* Asigna memoria para el numero total de matrices dentro del documento */
+MATRIZ asignar_memoria(int N) 
 {
-
+    return (MATRIZ*) malloc(sizeof(MATRIZ) * N);
 }
 
+/* Asigna memoria para cada una de las matrices y el numero de sus elementos */
+void alojar_memoria(MATRIZ *matriz, int m, int n)
+{
+    matriz->m = m;
+    matriz->n = n;
+
+    matriz->M = (double**) malloc(sizeof(double*) * matriz->m);
+
+    for (int i = 0; i < matriz->m; i++) {
+        matriz->M[i] = (double*) malloc(sizeof(double) * matriz->n);
+    }
+}
+
+/* Hacer lectura de todos los elementos de las matrices */
 void leer_matriz(MATRIZ *matriz)
 {
-
+    int i,j;
+    for (i = 0; i < matriz->m; i++) {
+        for (j = 0; j < matriz->n; j++) {
+            scanf("%lf", &matriz->M[i][j]);
+        }
+    }
 }
 
 void llenar_matriz(MATRIZ *matriz)
@@ -32,7 +54,12 @@ void llenar_matriz(MATRIZ *matriz)
 
 }
 
+/* Libera el espacio en memoria que se acupo para las matrices */
 void liberar_memoria(MATRIZ *matriz)
 {
-    
+    for(int i = 0; i < matriz->m; i++) {
+        free(matriz->M[i]);
+    }
+
+    free(matriz->M);
 }
