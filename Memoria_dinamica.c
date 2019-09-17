46f4cf52638e21f5be95b7edc3d6b54e33c8f77b
Antonio Guzmán <agvcoder@gmail.com>
2019-09-17 08:52:01 -0500

    Añadiendo estructura y cabeceras principales para la memoria dinamica

--- Memoria_dinamica.c
+++ Memoria_dinamica.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
+typedef struct
+{
+    double **M;
+    int m, n;
+}MATRIZ;
+
+void alojar_memoria(MATRIZ *matriz, int m, int n);
+void leer_matriz(MATRIZ *matriz);
+void llenar_matriz(MATRIZ *matriz);
+void liberar_memoria(MATRIZ *matriz);
+
 int main(int argc, char const *argv[])
 {
     /* code */
     return 0;
+}
+
+void alojar_memoria(MATRIZ *matriz, int m, int n)
+{
+
+}
+
+void leer_matriz(MATRIZ *matriz)
+{
+
+}
+
+void llenar_matriz(MATRIZ *matriz)
+{
+
+}
+
+void liberar_memoria(MATRIZ *matriz)
+{
+    
 }
